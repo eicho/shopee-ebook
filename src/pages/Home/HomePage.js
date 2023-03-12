dcb6601c0e16490faaa6e999eb19a5b9db1e1358
Ei Cho <32412506+eicho@users.noreply.github.com>
2023-03-12 14:39:15 +0630

    create accordion section

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -1,6 +1,7 @@
 import { Hero } from "./components/Hero";
 import { FeaturedProducts } from "./components/FeaturedProducts";
 import { Testimonials } from "./components/Testimonials";
+import { Faq } from "./components/Faq";
 
 export const HomePage = () => {
   return (
@@ -8,6 +9,7 @@ export const HomePage = () => {
       <Hero />
       <FeaturedProducts />
       <Testimonials />
+      <Faq />
     </main>
   );
 };
