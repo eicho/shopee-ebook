986f0bb4bf650f78a93b71f1d4fec027148a9cce
Ei Cho <32412506+eicho@users.noreply.github.com>
2023-03-12 13:31:25 +0630

    create Featured eBooks section

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -1,9 +1,11 @@
 import { Hero } from "./components/Hero";
+import { FeaturedProducts } from "./components/FeaturedProducts";
 
 export const HomePage = () => {
   return (
     <main>
       <Hero />
+      <FeaturedProducts />
     </main>
   );
 };
