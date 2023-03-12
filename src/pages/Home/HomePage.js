e26416eb60507eda9f19200cc919cc603d234772
Ei Cho <32412506+eicho@users.noreply.github.com>
2023-03-12 13:38:27 +0630

    create 'Testimonial' section

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -1,11 +1,13 @@
 import { Hero } from "./components/Hero";
 import { FeaturedProducts } from "./components/FeaturedProducts";
+import { Testimonials } from "./components/Testimonials";
 
 export const HomePage = () => {
   return (
     <main>
       <Hero />
       <FeaturedProducts />
+      <Testimonials />
     </main>
   );
 };
