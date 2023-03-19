db7fa0f1ee840ef9eda57bb73087d54bf6c104d8
Ei Cho <32412506+eicho@users.noreply.github.com>
2023-03-19 21:12:21 +0630

    Toggle Dropdown (Hide)

--- src/components/Elements/DropdownLoggedIn.js
+++ src/components/Elements/DropdownLoggedIn.js
@@ -1,11 +1,12 @@
 import { Link, useNavigate } from "react-router-dom";
 
-export const DropdownLoggedIn = () => {
+export const DropdownLoggedIn = ({ setDropdown }) => {
   const navigate = useNavigate();
 
   function handleLogout() {
     sessionStorage.removeItem("token");
     sessionStorage.removeItem("cbid");
+    setDropdown(false);
     navigate("/");
   }
 
@@ -23,6 +24,7 @@ export const DropdownLoggedIn = () => {
       >
         <li>
           <Link
+            onClick={() => setDropdown(false)}
             to="/products"
             className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
           >
@@ -31,6 +33,7 @@ export const DropdownLoggedIn = () => {
         </li>
         <li>
           <Link
+            onClick={() => setDropdown(false)}
             to="/dashboard"
             className="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
           >
