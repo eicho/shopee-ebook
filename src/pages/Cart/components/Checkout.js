0d337d9350933e3afce0d4b82d97050f8b42bfe6
Ei Cho <32412506+eicho@users.noreply.github.com>
2023-03-21 19:27:02 +0630

    Condition Check Already In Cart

--- src/pages/Cart/components/Checkout.js
+++ src/pages/Cart/components/Checkout.js
@@ -1,4 +1,8 @@
+import { useCart } from "../../../context";
+
 export const Checkout = ({ setCheckout }) => {
+  const { total } = useCart();
+
   return (
     <section>
       <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50"></div>
@@ -49,7 +53,7 @@ export const Checkout = ({ setCheckout }) => {
                     name="name"
                     id="name"
                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:value-gray-400 dark:text-white"
-                    value="Shubham Sarda"
+                    value="Admin"
                     disabled
                     required=""
                   />
@@ -66,7 +70,7 @@ export const Checkout = ({ setCheckout }) => {
                     name="email"
                     id="email"
                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:value-gray-400 dark:text-white"
-                    value="shubham@example.com"
+                    value="admin@example.com"
                     disabled
                     required=""
                   />
@@ -132,7 +136,7 @@ export const Checkout = ({ setCheckout }) => {
                   />
                 </div>
                 <p className="mb-4 text-2xl font-semibold text-lime-500 text-center">
-                  $99
+                  ${total}
                 </p>
                 <button
                   type="submit"
